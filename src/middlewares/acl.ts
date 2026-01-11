3262885cfcc4a65c3984356ca712fab801950bc1
Mazyaa <muhamadazisudarya@unbaja.ac.id>
2026-01-11 16:50:11 +0700

    fix: fixing typing for role

--- src/middlewares/acl.ts
+++ src/middlewares/acl.ts
@@ -1,10 +1,10 @@
 import { NextFunction, Request, Response } from "express";
-import { ROLES } from "../utils/constants";
+import { RoleUser } from "@prisma/client";
 import { HttpError } from "../utils/error";
 
-const alloRoles = (...roles: ROLES[]) => {
+const alloRoles = (...roles: RoleUser[]) => {
     return (req: Request, res: Response, next: NextFunction): void => {
-        const userRole = res.locals.user.role as ROLES;
+        const userRole = res.locals.user.role as RoleUser;
 
         if (!roles.includes(userRole)) {
             throw new HttpError("Forbidden: You don't have permission to access this resource", 403);
@@ -14,8 +14,10 @@ const alloRoles = (...roles: ROLES[]) => {
     }
 }
 
-export const isAdmin = alloRoles(ROLES.ADMIN);
-export const isInstrukturOrAsesor = alloRoles(ROLES.INSTRUKTUR, ROLES.ASESOR);
-export const isDirektur = alloRoles(ROLES.DIREKTUR);
-export const isPeserta = alloRoles(ROLES.PESERTA);
-export const isAdminOrDirektur = alloRoles(ROLES.ADMIN, ROLES.DIREKTUR);
+export const isAdmin = alloRoles(RoleUser.admin);
+export const isInstrukturOrAsesor = alloRoles(RoleUser.instruktur, RoleUser.asesor);
+export const isInstruktur = alloRoles(RoleUser.instruktur);
+export const isAsesor = alloRoles(RoleUser.asesor);
+export const isPeserta = alloRoles(RoleUser.peserta);
+export const isDirektur = alloRoles(RoleUser.direktur);
+export const isAdminOrDirektur = alloRoles(RoleUser.admin, RoleUser.direktur);
