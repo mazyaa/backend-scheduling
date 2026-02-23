968404bef169fbcdf8b7c018297d6f2ab3204bd4
Mazyaa <muhamadazisudarya@unbaja.ac.id>
2026-02-23 14:30:26 +0700

    fix: fixing deployment

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,4 @@
-import { Application, Router } from 'express';
+import { Application, Request, Response, Router } from 'express';
 import authRoutes from './auth';
 import instrukturOrAsesorRoutes from './instrukturOrAsesor';
 import trainingRoutes from './training';
@@ -7,21 +7,27 @@ import { detailScheduleRoutes } from './detailSchedule';
 import { notificationRoutes } from './notification';
 import { credentialRoutes } from './credential';
 
-
 const routes = (app: Application): void => {
   const router = Router(); // this value is null at the beginning
 
   // to read it, if user access routes with /api, it will use this router
   app.use('/api', router); // second params is use for registering all routes to /api path
 
+  app.use('/', (_req: Request, res: Response) => {
+    res.status(200).json({
+      message: 'Welcome to the API SCHEDULING and E-CERTIFICATE!',
+      data: null,
+    });
+  });
+
   // registering all routes to router
-  authRoutes(router); 
+  authRoutes(router);
   instrukturOrAsesorRoutes(router);
   trainingRoutes(router);
   scheduleRoutes(router);
   detailScheduleRoutes(router);
   notificationRoutes(router);
-  credentialRoutes(router)
+  credentialRoutes(router);
 };
 
 export default routes;
