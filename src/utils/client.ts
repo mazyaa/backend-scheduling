2960a1d8b72cff346228a1d631f38ab5da5ec63f
Mazyaa <muhamadazisudarya@unbaja.ac.id>
2026-02-23 13:35:43 +0700

    prod: created setup for production ready

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -1,8 +1,8 @@
 import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { DATABASE_URL } from './env';
+import { DATABASE_URL_PRODUCTION } from './env';
 
-const connectionString = DATABASE_URL;
+const connectionString = DATABASE_URL_PRODUCTION;
 
 const adapter = new PrismaPg({ connectionString }); // use adapter for connection to PostgreSQL
 export const prisma = new PrismaClient({ adapter }); // initialize Prisma Client with the adapter
