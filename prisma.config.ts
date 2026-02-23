6fe91229ea856dc00f6f5104222f3cc99e08cbaa
Mazyaa <muhamadazisudarya@unbaja.ac.id>
2026-02-23 16:05:56 +0700

    chore: fixing prisma

--- prisma.config.ts
+++ prisma.config.ts
@@ -7,7 +7,4 @@ export default defineConfig({
   migrations: {
     path: "prisma/migrations",
   },
-  datasource: {
-    url: process.env.DATABASE_URL, // use environment variable for database connection string
-  }
 });
