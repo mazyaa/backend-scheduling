11d0a1dc745e24b14c55c88677ae62b863bd2ba3
Mazyaa <muhamadazisudarya@unbaja.ac.id>
2026-02-23 21:16:58 +0700

    refactor: refactoring main of app

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import { createServer, Server } from 'http';
 import config from './config';
-import app from './app';
-
+import express, { Express } from 'express';
+import loadMiddlewares from './loaders';
+import routes from './routes';
+import { loadErrorMiddlewares } from './middlewares/error';
 
 async function main(): Promise<void> {
   try {
+    const app: Express = express(); // create an express application
+
     const server: Server = createServer(app); // create an HTTP server using the express app
+
+    loadMiddlewares(app);
+    routes(app);
+    loadErrorMiddlewares(app);
 
     server.listen(config.port, () => {
       console.log(
