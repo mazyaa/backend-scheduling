1b51eb77ac96b9d59c27e514142bf0b64a926372
Mazyaa <muhamadazisudarya@unbaja.ac.id>
2026-03-28 20:35:10 +0700

    chore: change DateTime on jadwal and detail jadwal schema to Date

---
+++ src/utils/prisma/migrations/20260328133346_date_time_to_date/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "detail_jadwal_training" ALTER COLUMN "hari" SET DATA TYPE DATE;
+
+-- AlterTable
+ALTER TABLE "jadwal_training" ALTER COLUMN "start_date" SET DATA TYPE DATE;
