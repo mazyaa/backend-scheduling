860ed59e0a3d8d0f3fe9c8ec995637ca8b1f466b
Mazyaa <muhamadazisudarya@unbaja.ac.id>
2026-07-31 15:45:14 +0700

    fix: fixing CORS

--- src/loaders/middlewareLoaders.ts
+++ src/loaders/middlewareLoaders.ts
@@ -2,26 +2,21 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
-import { Application } from 'express'; // type inside application express is a object with free structure (key: string, value: any)
+import { Application } from 'express'; // type inside application express is a object with free structure (key: string, value: any) 
 
 const loadMiddlewares = (app: Application): void => {
-  app.use(
-    cors({
-      origin: [
-        'http://localhost:3000',
-        'https://frontend-scheduling-production-45ee.up.railway.app',
-      ],
-      credentials: true, // allow cookies to be sent with requests
-    }),
-  );
+    app.use(cors({ 
+        origin: process.env.FRONTEND_URL,
+        credentials: true // allow cookies to be sent with requests
+    }));
 
-  app.use(express.json()); // for parsing application/json (AJAX requests)
+    app.use(express.json()); // for parsing application/json (AJAX requests)
 
-  app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded (HTML form submissions)
+    app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded (HTML form submissions)
 
-  app.use(morgan('combined')); // logging middleware
+    app.use(morgan('combined')); // logging middleware
 
-  app.use(cookieParser()); // for parsing cookies (req.cookies)
-};
+    app.use(cookieParser()); // for parsing cookies (req.cookies)
+} 
 
 export default loadMiddlewares;
